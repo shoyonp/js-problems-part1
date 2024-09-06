06e8abbbbbe2fb068dda756e24b3505669a8d0e7
shoyonp <shoyonkumar143@gmail.com>
2024-09-07 01:24:54 +0600

    convert done

--- convert.js
+++ convert.js
@@ -5,5 +5,26 @@ function inchToFeet(inch){
     return feet;
 }
 
+// ignorre
+function inchToFeet2(inch){
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch %  12;
+    const result = feetNumber + ' ft ' + inchRemaining + ' inch.'
+    return result
+}
+
 const height = inchToFeet(75);
-console.log(height);
+// console.log(height);
+const height2 = inchToFeet2(75);
+// console.log(height2);
+
+function mileToKilometer (mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+}
+
+function KilometersToMiles(kilo){
+    const mile = kilo * 0.621371;
+    return mile;
+}
